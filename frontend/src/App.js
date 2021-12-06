3d6798f6597ce595c742ae31d170738ee9017ff8
Sourabh Bucha <84260075+sourabhbucha@users.noreply.github.com>
2021-12-06 16:03:00 +0530

    first

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import {Container} from 'react-bootstrap';
+import Footer from './Components/Footer';
+
+function App() {
+  return (
+    <>
+      <main>
+        <Container>
+          <h1>ECommerce app</h1>
+        </Container>
+      </main>
+      
+      <Footer />
+    </>
+
+  );
+}
+
+export default App;
